afa497c3335da0f7882077092fa16622562f5378
Robert Mayr <robyduck@fedoraproject.org>
2013-11-14 23:57:54 +0100

    adding OSP DIN font and update CSS

--- lgm2014/header.php
+++ lgm2014/header.php
@@ -14,6 +14,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/osp-din" rel="stylesheet" type="text/css"/>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
