d0e4a2f0d1f0f5bb1fc01eef60e77218725ee672
Robert Mayr <robyduck@fedoraproject.org>
2013-11-16 23:55:57 +0100

    add social buttons to the footer

--- lgm2014/footer.php
+++ lgm2014/footer.php
@@ -11,6 +11,23 @@
 	</div><!-- #main -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="social">
+			<dl>
+				<dd>
+				<a href="https://www.facebook.com/groups/libregraphicsmeeting/">
+				<img alt="Follow LGM on Facebook" src="./wp-content/themes/lgm2014/images/facebook.png">
+				</a>
+				<a href="https://twitter.com/LGMLeipzig">
+				<img alt="Follow LGM on Twitter" src="./wp-content/themes/lgm2014/images/twitter.png">
+				</a>
+				<a href="https://plus.google.com/u/0/110964138103273662022">
+				<img alt="Follow LGM on Google+" src="./wp-content/themes/lgm2014/images/gplus.png">
+				<a href="http://www.flickr.com/groups/libregfx/pool/with/2493672905/">
+				<img alt="Follow LGM on Flickr" src="./wp-content/themes/lgm2014/images/flickr.png">
+				</a>
+				</dd>
+			</dl>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
