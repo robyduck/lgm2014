59e7d2c88c54c039caaa55d2ab26e2772ec84252
Robert Mayr <robyduck@fedoraproject.org>
2013-11-21 11:31:11 +0100

    make social links relative

--- lgm2014/footer.php
+++ lgm2014/footer.php
@@ -15,15 +15,15 @@
 			<dl>
 				<dd>
 				<a href="https://www.facebook.com/groups/libregraphicsmeeting/">
-				<img alt="Follow LGM on Facebook" src="./wp-content/themes/lgm2014/images/facebook.png">
+				<img alt="Follow LGM on Facebook" src="<?=get_template_directory_uri() ?>/images/facebook.png">
 				</a>
 				<a href="https://twitter.com/LGMLeipzig">
-				<img alt="Follow LGM on Twitter" src="./wp-content/themes/lgm2014/images/twitter.png">
+				<img alt="Follow LGM on Twitter" src="<?=get_template_directory_uri() ?>/images/twitter.png">
 				</a>
 				<a href="https://plus.google.com/u/0/110964138103273662022">
-				<img alt="Follow LGM on Google+" src="./wp-content/themes/lgm2014/images/gplus.png">
+				<img alt="Follow LGM on Google+" src="<?=get_template_directory_uri() ?>/images/gplus.png">
 				<a href="http://www.flickr.com/groups/libregfx/pool/with/2493672905/">
-				<img alt="Follow LGM on Flickr" src="./wp-content/themes/lgm2014/images/flickr.png">
+				<img alt="Follow LGM on Flickr" src="<?=get_template_directory_uri() ?>/images/flickr.png">
 				</a>
 				</dd>
 			</dl>
